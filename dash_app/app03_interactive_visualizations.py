bc25f69c389e1ecdef5e185f7a415ecdeb7dd3d4
Ole Bause <ole@bause.me>
2023-05-11 11:53:07 +0200

    interactive visualizations

--- dash_app/app03_interactive_visualizations.py
+++ dash_app/app03_interactive_visualizations.py
@@ -4,6 +4,7 @@ from dash import Dash, dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -23,6 +24,13 @@ df = pd.DataFrame({
     "fruit": ["apple", "apple", "orange", "orange"]
 })
 
+df2 = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+# make a sample data frame with 6 columns
+np.random.seed(0)  # no-display
+df3 = pd.DataFrame({"Col " + str(i + 1): np.random.rand(30) for i in range(6)})
+
+
 fig = px.scatter(df, x="x", y="y", color="fruit", custom_data=["customdata"])
 
 fig.update_layout(clickmode='event+select')
@@ -30,6 +38,10 @@ fig.update_layout(clickmode='event+select')
 fig.update_traces(marker_size=20)
 
 app.layout = html.Div([
+    html.H1("Interactive Visualizations"),
+    html.Hr(),
+    html.Br(),
+    html.H3("Basic Interactions"),
     dcc.Graph(
         id='basic-interactions',
         figure=fig
@@ -79,10 +91,83 @@ app.layout = html.Div([
             """),
             html.Pre(id='relayout-data', style=styles['pre']),
         ], className='three columns')
-    ])
+    ]),
+    
+    html.H3("Update Graphs on Hover"),
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                df2['Indicator Name'].unique(),
+                'Fertility rate, total (births per woman)',
+                id='crossfilter-xaxis-column',
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-xaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ],
+        style={'width': '49%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                df2['Indicator Name'].unique(),
+                'Life expectancy at birth, total (years)',
+                id='crossfilter-yaxis-column'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='crossfilter-yaxis-type',
+                labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            )
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
+    ], style={
+        'padding': '10px 5px'
+    }),
+
+    html.Div([
+        dcc.Graph(
+            id='crossfilter-indicator-scatter',
+            hoverData={'points': [{'customdata': 'Japan'}]}
+        )
+    ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
+    html.Div([
+        dcc.Graph(id='x-time-series'),
+        dcc.Graph(id='y-time-series'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
+
+    html.Div(dcc.Slider(
+        df2['Year'].min(),
+        df2['Year'].max(),
+        step=None,
+        id='crossfilter-year--slider',
+        value=df2['Year'].max(),
+        marks={str(year): str(year) for year in df2['Year'].unique()}
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'}),
+    
+    html.H3("Generic Crossfilter Recipe"),	
+    html.Div(
+    [
+        html.Div(
+            dcc.Graph(id="g1", config={"displayModeBar": False}),
+            className="four columns",
+        ),
+        html.Div(
+            dcc.Graph(id="g2", config={"displayModeBar": False}),
+            className="four columns",
+        ),
+        html.Div(
+            dcc.Graph(id="g3", config={"displayModeBar": False}),
+            className="four columns",
+        ),
+    ],
+    className="row"),
 ])
 
-
+# Basic Interactions
 @app.callback(
     Output('hover-data', 'children'),
     Input('basic-interactions', 'hoverData'))
@@ -110,6 +195,153 @@ def display_selected_data(selectedData):
 def display_relayout_data(relayoutData):
     return json.dumps(relayoutData, indent=2)
 
+# Update Graphs on Hover
+@app.callback(
+    Output('crossfilter-indicator-scatter', 'figure'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'),
+    Input('crossfilter-yaxis-type', 'value'),
+    Input('crossfilter-year--slider', 'value'))
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df2[df2['Year'] == year_value]
+
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+            y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+            hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name']
+            )
+
+    fig.update_traces(customdata=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
+
+    fig.update_xaxes(title=xaxis_column_name, type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name, type='linear' if yaxis_type == 'Linear' else 'log')
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    return fig
+
+
+def create_time_series(dff, axis_type, title):
+
+    fig = px.scatter(dff, x='Year', y='Value')
+
+    fig.update_traces(mode='lines+markers')
+
+    fig.update_xaxes(showgrid=False)
+
+    fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
+
+    fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       text=title)
+
+    fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
+
+    return fig
+
+
+@app.callback(
+    Output('x-time-series', 'figure'),
+    Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-xaxis-column', 'value'),
+    Input('crossfilter-xaxis-type', 'value'))
+def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
+    country_name = hoverData['points'][0]['customdata']
+    dff = df2[df2['Country Name'] == country_name]
+    dff = dff[dff['Indicator Name'] == xaxis_column_name]
+    title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
+    return create_time_series(dff, axis_type, title)
+
+
+@app.callback(
+    Output('y-time-series', 'figure'),
+    Input('crossfilter-indicator-scatter', 'hoverData'),
+    Input('crossfilter-yaxis-column', 'value'),
+    Input('crossfilter-yaxis-type', 'value'))
+def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
+    dff = df2[df2['Country Name'] == hoverData['points'][0]['customdata']]
+    dff = dff[dff['Indicator Name'] == yaxis_column_name]
+    return create_time_series(dff, axis_type, yaxis_column_name)
+
+# Generic Crossfilter Recipe
+def get_figure(df, x_col, y_col, selectedpoints, selectedpoints_local):
+
+    if selectedpoints_local and selectedpoints_local["range"]:
+        ranges = selectedpoints_local["range"]
+        selection_bounds = {
+            "x0": ranges["x"][0],
+            "x1": ranges["x"][1],
+            "y0": ranges["y"][0],
+            "y1": ranges["y"][1],
+        }
+    else:
+        selection_bounds = {
+            "x0": np.min(df[x_col]),
+            "x1": np.max(df[x_col]),
+            "y0": np.min(df[y_col]),
+            "y1": np.max(df[y_col]),
+        }
+
+    # set which points are selected with the `selectedpoints` property
+    # and style those points with the `selected` and `unselected`
+    # attribute. see
+    # https://medium.com/@plotlygraphs/notes-from-the-latest-plotly-js-release-b035a5b43e21
+    # for an explanation
+    fig = px.scatter(df, x=df[x_col], y=df[y_col], text=df.index)
+
+    fig.update_traces(
+        selectedpoints=selectedpoints,
+        customdata=df.index,
+        mode="markers+text",
+        marker={"color": "rgba(0, 116, 217, 0.7)", "size": 20},
+        unselected={
+            "marker": {"opacity": 0.3},
+            "textfont": {"color": "rgba(0, 0, 0, 0)"},
+        },
+    )
+
+    fig.update_layout(
+        margin={"l": 20, "r": 0, "b": 15, "t": 5},
+        dragmode="select",
+        hovermode=False,
+        newselection_mode="gradual",
+    )
+
+    fig.add_shape(
+        dict(
+            {"type": "rect", "line": {"width": 1, "dash": "dot", "color": "darkgrey"}},
+            **selection_bounds
+        )
+    )
+    return fig
+
+
+# this callback defines 3 figures
+# as a function of the intersection of their 3 selections
+@app.callback(
+    Output("g1", "figure"),
+    Output("g2", "figure"),
+    Output("g3", "figure"),
+    Input("g1", "selectedData"),
+    Input("g2", "selectedData"),
+    Input("g3", "selectedData"),
+)
+def callback(selection1, selection2, selection3):
+    selectedpoints = df3.index
+    for selected_data in [selection1, selection2, selection3]:
+        if selected_data and selected_data["points"]:
+            selectedpoints = np.intersect1d(
+                selectedpoints, [p["customdata"] for p in selected_data["points"]]
+            )
+
+    return [
+        get_figure(df3, "Col 1", "Col 2", selectedpoints, selection1),
+        get_figure(df3, "Col 3", "Col 4", selectedpoints, selection2),
+        get_figure(df3, "Col 5", "Col 6", selectedpoints, selection3),
+    ]
 
 if __name__ == '__main__':
     app.run_server(debug=True)
